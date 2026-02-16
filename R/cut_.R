e056c82fe28d296972c8dbd6a82534396635acce
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-15 21:38:12 -0500

    tzh

--- R/cut_.R
+++ R/cut_.R
@@ -17,6 +17,8 @@
 #' R \link[base]{language} is also accepted.
 #' Default is the argument call of `x`
 #' 
+#' @param fmt see functions [cut_levels()] and \link[base]{sprintf}
+#' 
 #' @param ... additional parameters `right` and `include.lowest` of 
 #' functions \link[base]{cut.default} and \link[base]{.bincode}.
 #' 
@@ -28,35 +30,34 @@
 #' \item {`-Inf` and `Inf` are added to `breaks`, 
 #' so that the values outside of `breaks` will be correctly categorized,
 #' instead of returning an `NA_integer` per \link[base]{.bincode}}
-#' \item {More user-friendly \link[base]{factor} \link[base]{levels}, see helper function [cut_levels]}
+#' \item {More user-friendly \link[base]{factor} \link[base]{levels}, see helper function [cut_levels()]}
 #' }
 #' 
 #' @examples 
-#' morley$Speed |> 
+#' datasets::morley$Speed |> 
 #'  cut_(breaks = 100*(8:9), data.name = 'speed') |> 
 #'  table()
-#' morley$Speed |> 
+#' datasets::morley$Speed |> 
 #'  cut_(probs = c(.3, .6)) |> 
 #'  table()
 #' 
-#' VADeaths |> 
-#'  cut_(probs = c(.3, .6))
+#' datasets::VADeaths |> 
+#'  cut_(probs = c(.3, .6), data.name = 'death', fmt = '%.3g\u2030')
 #' 
-#' JohnsonJohnson |> 
+#' datasets::JohnsonJohnson |> 
 #'  zoo::as.Date.ts() |>
 #'  cut_(breaks = as.Date(c('1970-01-01', '1975-01-01')), 
 #'    data.name = 'JnJ') |>
 #'  table()
 #' 
-#' @keywords internal  
-#' @name cut_
-#' @importFrom stats quantile
+#' @keywords internal
 #' @export
 cut_ <- function(
     x, 
     breaks = quantile(x, probs = probs, na.rm = TRUE), 
     probs,
     data.name = substitute(x),
+    fmt = '%.3g',
     ...
 ) {
   
@@ -69,14 +70,14 @@ cut_ <- function(
   
   breaks <- c(breaks, -Inf, Inf) |> # `breaks` before `Inf` and `-Inf`, then the returned value has attributes of `breaks`
     unique() |> # dont want to use my [unique_allequal]
-    sort() # *not* ?base::sort.int # I need to retain the class of `breaks`
+    sort() # *not* ?base::sort.int # I need to retain the attr of `breaks`
   
   ret <- x
   storage.mode(ret) <- 'integer'
   ret[] <- .bincode(x, breaks = breaks, ...) # 'integer'
   # keeps `attributes(x)`
   
-  attr(ret, which = 'levels') <- cut_levels(breaks = breaks, ..., data.name = data.name)
+  attr(ret, which = 'levels') <- cut_levels(breaks = breaks, ..., fmt = fmt, data.name = data.name)
   class(ret) <- 'factor'
   return(ret)
 
@@ -89,38 +90,30 @@ cut_ <- function(
 
 #' @title [cut_levels()]
 #' 
-#' @param breaks \link[base]{vector} of the same \link[base]{class} as `x`. 
-#' `-Inf` and `Inf` will be automatically added
+#' @param breaks,right,include.lowest see functions \link[base]{cut.default} and \link[base]{.bincode}.
 #' 
-#' @param right \link[base]{logical} scalar, default `TRUE`, 
-#' see functions \link[base]{cut.default} and \link[base]{.bincode}.
+#' @param fmt see function \link[base]{sprintf}
 #' 
-#' @param include.lowest \link[base]{logical} scalar, default `TRUE`, 
-#' see functions \link[base]{cut.default} and \link[base]{.bincode}.
-#' 
-#' @param data.name \link[base]{character} scalar, name of data.
-#' R \link[base]{language} is also accepted.
-#' Default is the argument call of `x`
+#' @param data.name \link[base]{character} scalar or \link[base]{language}, see function [cut_()]
 #' 
 #' @examples 
-#' ## Examples on Helper Function cut_levels()
 #' foo = \(...) {
 #'  cbind(
 #'   numeric(0) |> cut.default(...) |> levels(),
 #'   cut_levels(...)
 #'  )
 #' }
-#' foo(breaks = 1:4, right = TRUE, include.lowest = TRUE)
-#' foo(breaks = 1:4, right = FALSE, include.lowest = TRUE)
-#' foo(breaks = 1:4, right = TRUE, include.lowest = FALSE)
-#' foo(breaks = 1:4, right = FALSE, include.lowest = FALSE)
-#' set.seed(29); foo(breaks = c(-Inf, sort(rnorm(1:3)), Inf))
+#' foo(breaks = c(-Inf, 1:4, Inf), right = TRUE, include.lowest = TRUE)
+#' foo(breaks = c(-Inf, 1:4, Inf), right = FALSE, include.lowest = TRUE)
+#' foo(breaks = c(-Inf, 1:4, Inf), right = TRUE, include.lowest = FALSE)
+#' foo(breaks = c(-Inf, 1:4, Inf), right = FALSE, include.lowest = FALSE)
 #' @keywords internal
 #' @export
 cut_levels <- function(
     breaks, 
     right = TRUE, 
     include.lowest = TRUE, 
+    fmt = '%.3g',
     data.name = 'x'
 ) {
   
@@ -128,15 +121,18 @@ cut_levels <- function(
   if (!is.character(data.name) || length(data.name) != 1L || is.na(data.name) || !nzchar(data.name)) stop('illegal `data.name`')
   nb <- length(breaks)
   
-  fmt_breaks <- trimws(if (inherits(breaks, what = 'Date')) {
-    breaks |> format() # use S3
+  b <- trimws(if (inherits(breaks, what = 'Date')) {
+    breaks |> 
+      format() # use S3
   } else if (is.integer(breaks)) {
-    breaks |> as.character.default()
+    breaks |> 
+      as.character.default()
   } else if (is.double(breaks)) {
-    breaks |> sprintf(fmt = '%.3g')
+    breaks |> 
+      sprintf(fmt = fmt)
   })
-  b1 <- fmt_breaks[1:(nb-1L)]
-  b2 <- fmt_breaks[2:nb]
+  b1 <- b[1:(nb-1L)]
+  b2 <- b[2:nb]
   
   if (right) {
     # `r`elationship
@@ -147,9 +143,15 @@ cut_levels <- function(
     r2 <- if (!include.lowest) '<' else c(rep('<', times = nb - 2L), '\u2264') 
   }
   
-  L <- gsub(pattern = '^-Inf\u2264|^-Inf<', replacement = '', x = paste0(b1, r1)) 
-  R <- gsub(pattern = '\u2264Inf$|<Inf$', replacement = '', x = paste0(r2, b2)) 
-  
+  L <- (-Inf) |> 
+    sprintf(fmt = fmt) |>
+    paste0('^', . = _, c('\u2264', '<'), collapse = '|') |>
+    gsub(pattern = _, replacement = '', x = paste0(b1, r1)) 
+  R <- Inf |> 
+    sprintf(fmt = fmt) |>
+    paste0(c('\u2264', '<'), . = _, '$', collapse = '|') |>
+    gsub(pattern = _, replacement = '', x = paste0(r2, b2)) 
+
   if (!nzchar(R[nr <- length(R)])) {
     # change last category from `a<X` or `a<=X` to `X>a` and `X>=a`
     L[nr] <- ''
